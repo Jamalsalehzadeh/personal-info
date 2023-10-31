72b06c5ec4b071abf35235aef908c3296464d280
Jamal <salehzadeh.jamal@gmail.com>
2023-10-31 14:55:10 +0330

    Renames the page

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,36 @@
 import React from 'react'
-import logo from './logo.svg'
+import avatar from './avatar.jpg'
 import './App.css'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          <b className="App-link">I</b>t<b className="App-link">I</b>s
-          <b className="App-link">A</b>ram.
-        </p>
-        <a
-          className="App-link"
-          href="https://jamalsalehzadeh.github.io/react-gantt-chart"
-          rel="noopener noreferrer"
+        <div style={{ width: 200, height: 200 }}>
+          <img src={avatar} className="App-logo" alt="Avatar" />
+        </div>
+        <div
+          style={{
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+            flexDirection: 'column',
+            marginLeft: 10,
+          }}
         >
-          About Me
-        </a>
+          <p>
+            <b className="App-link">I</b>t<b className="App-link">I</b>s
+            <b className="App-link">A</b>ram.
+          </p>
+          <a
+            className="App-link"
+            href="https://jamalsalehzadeh.github.io/personal-info"
+            rel="noopener noreferrer"
+          >
+            Under Construction
+          </a>
+        </div>
+        <div></div>
       </header>
     </div>
   )
