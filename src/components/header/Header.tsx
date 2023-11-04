90575b75255c7dce1e158b67731a564556903224
ItIsAram <j.salehzadeh@targetofs.com>
2023-11-04 16:38:08 +0330

    Init header

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,4 +1,5 @@
 import headerBackground from '../../resources/images/header-background.png'
+import avatarImage from '../../resources/images/itisaram.png'
 import './header.css'
 
 export const Header = () => {
@@ -10,6 +11,46 @@ export const Header = () => {
         src={headerBackground}
         alt="Header Background"
       />
+      <div className="header-imageContainer">
+        <img
+          className="header-imageContainer-avatar"
+          src={avatarImage}
+          alt="It Is Aram"
+        />
+      </div>
+      <div className="header-descriptionContainer">
+        <div className="header-descriptionContainer-description">
+          <p className="header-text">Hey, my name is</p>
+          <p
+            className="header-text"
+            style={{
+              fontSize: '2rem',
+              fontWeight: 'bolder',
+              color: '#000000',
+            }}
+          >
+            JAMAL
+            <br />
+            SALEHZADEH
+          </p>
+          <p className="header-text">Family and friends Call me</p>
+          <p
+            className="header-text"
+            style={{
+              fontSize: '3rem',
+              fontWeight: 'bold',
+              color: '#9f0000',
+            }}
+          >
+            ARAM
+          </p>
+          <p className="header-text">
+            I am a front end developer. Studied at Petroleum Engineering but I
+            belong to codes not oils...
+          </p>
+          <p className="header-text">30 years old, happy and married.</p>
+        </div>
+      </div>
     </div>
   )
 }
